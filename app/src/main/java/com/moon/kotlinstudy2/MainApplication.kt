46548444bdb047054266b6ae0898aa9818d08d66
문 성 운 <mkik90911@naver.com>
2022-02-23 18:55:36 +0900

    Kotlin iateinit AND companion object comment

--- app/src/main/java/com/moon/kotlinstudy2/MainApplication.kt
+++ app/src/main/java/com/moon/kotlinstudy2/MainApplication.kt
@@ -5,11 +5,12 @@ import android.content.Context
 
 class MainApplication : Application() {
     init {
-        instance = this
+        instance = this //MainApplication 클래스가 초기화될때 instance를 초기화해줌
+
     }
 
     companion object{
-        lateinit var instance:MainApplication
+         lateinit var instance:MainApplication //늦은 초기화를 위해 iateinit 선언
 
         fun getContext():Context {
             return instance.applicationContext
--- app/src/main/java/com/moon/kotlinstudy2/ToastUtil.kt
+++ app/src/main/java/com/moon/kotlinstudy2/ToastUtil.kt
@@ -4,9 +4,11 @@ import android.widget.Toast
 
  //코틀린의 최상위 함수, 클래스 외부에 선언하여 패키지의 전역적으로 사용가능
 fun toastShort(str:String){
-    Toast.makeText(MainApplication.getContext(),str,Toast.LENGTH_SHORT).show()
+     //둘가 같음 원래 Companion 키워드가 있어야함
+    Toast.makeText(MainApplication.Companion.getContext(),str,Toast.LENGTH_SHORT).show()
 }
 fun toastLong(str:String){
+    //위에서 Companion이 생략되었지만 정상동작함
     Toast.makeText(MainApplication.getContext(),str,Toast.LENGTH_LONG).show()
 }
 class ToastUtil {
